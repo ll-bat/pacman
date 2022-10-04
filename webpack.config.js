62f06f1bfac6613a2a930dff7650a798ab3525be
vladimer <gshatakishvili@gmail.com>
2022-10-04 12:55:23 +0400

    Pass mode to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path')
 
-module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: './src/app.js',
-    output: {
-        filename: 'build.js',
-        path: path.join(__dirname, 'dist')
-    },
-    watch: true,
+module.exports = env => {
+    console.log(env)
+    return {
+        mode: 'development',
+        devtool: 'source-map',
+        entry: './src/app.js',
+        output: {
+            filename: 'build.js',
+            path: path.join(__dirname, 'dist')
+        },
+        watch: env.mode === "development" ? true : false,
+    }
 }
